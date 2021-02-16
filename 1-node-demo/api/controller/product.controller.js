b2b9a79019c271dab862fb133e34e23a89fe3b4e
nghia191 <mai.nam.nghia.bunbu@gmail.com>
2021-02-16 09:45:59 +0700

    Rest API

---
+++ 1-node-demo/api/controller/product.controller.js
@@ -0,0 +1,12 @@
+const Product = require('../../models/product.model');
+
+module.exports = {
+    index: async (req, res) => {
+        const products = await Product.find();
+        res.json(products);
+    },
+    create: async (req, res) => {
+        const product = await Product.create(req.body);
+        res.json(product);
+    }
+}
--- 1-node-demo/index.js
+++ 1-node-demo/index.js
@@ -11,12 +11,14 @@ const bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 const authMiddleware = require('./middlewares/auth.middleware');
 const sessionMiddleware = require('./middlewares/session.middleware');
+const productAPI = require('./api/router/product.router');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cookieParser('mahsdjaskdask'))
 app.use(express.static('public'));
 app.use(sessionMiddleware);
+app.use('/api/products', productAPI);
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL);
